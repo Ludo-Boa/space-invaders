ea1f2777d8ac895870fa7b63b17cb92d061d3a61
ludovic borbolla <ludovic.borbolla@cyberiance.com>
2021-04-16 13:47:33 +0200

    début du sprite

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,23 @@
 
-function Sprite(filename, left, top) {
-   this._node = document.createElement("img");
-   this._node = filename;
-   this._node.style.position = "absolute";
-   document.body.appenChild(this._node);
-}
+class Sprite {
+   constructor(filename, id, left, top) {
+      this._node = document.createElement("img");
+      this._node.setAttribute("id", id); 
+      this._node.src = filename;
+      this._node.style.position = "absolute";
+      // this._node.style.left = left+"px";
+      // this._node.style.top = top+"px";
+      document.body.appendChild(this._node);
 
+      Object.defineProperty(this, "left", {
+         get: function(){
+            return this._left;
+         },
+         set: function(value){
+            this._left = value;
+            this._node.style.left = value + "px";
+         }
+      });
+   }
+   
+}
